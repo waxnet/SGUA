8aa4a4d99072142c7fbb8f6a10653300ad308e3f
waxnet <88239635+waxnet@users.noreply.github.com>
2023-12-12 22:51:48 +0100

    update
    updated for latest game version

--- offsets.h
+++ offsets.h
@@ -3,7 +3,7 @@
 uintptr_t gameAssembly = (uintptr_t)GetModuleHandle("GameAssembly.dll");
 
 // locker methods
-uintptr_t ownsAnimation = 0x5A4250;
-uintptr_t ownsEmote = 0x5A51D0;
-uintptr_t ownsFootstep = 0x5A60D0;
-uintptr_t ownsSkin = 0x5A6F10;
+uintptr_t ownsAnimation = 0x5BE5F0;
+uintptr_t ownsEmote = 0x5BF570;
+uintptr_t ownsFootstep = 0x5C0470;
+uintptr_t ownsSkin = 0x5C12B0;
